45cf3c0e227cbe787c16a2ddadd51c1ecff542f9
Ravan <ravanhashimov@gmail.com>
2023-07-01 05:17:39 -0700

    Refactor Product component

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,11 +1,6 @@
 import { Product } from "../../app/models/product";
-import {
-  Avatar, Button,
-  List,
-  ListItem,
-  ListItemAvatar,
-  ListItemText,
-} from "@mui/material";
+import { Button } from "@mui/material";
+import ProductList from "./ProductList";
 
 interface Props {
   products: Product[];
@@ -15,19 +10,10 @@ interface Props {
 export default function Catalog({ products, addProduct }: Props) {
   return (
     <>
-      <List>
-        {products.map((product) => (
-          <ListItem key={product.id}>
-            <ListItemAvatar>
-              <Avatar src={product.pictureUrl} />
-            </ListItemAvatar>
-            <ListItemText>
-              {product.name} - {product.price}
-            </ListItemText>
-          </ListItem>
-        ))}
-      </List>
-      <Button variant="contained" onClick={addProduct}>Add Product</Button>
+      <ProductList products={products} />
+      <Button variant="contained" onClick={addProduct}>
+        Add Product
+      </Button>
     </>
   );
 }
