10a74d36b266679b0c4286d496cad257b39fd330
Ravan <ravanhashimov@gmail.com>
2023-07-01 04:52:50 -0700

    Add images and MUI styles

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,21 +1,33 @@
 import { Product } from "../../app/models/product";
+import {
+  Avatar, Button,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+} from "@mui/material";
 
 interface Props {
   products: Product[];
   addProduct: () => void;
 }
 
-export default function Catalog({products, addProduct}: Props) {
+export default function Catalog({ products, addProduct }: Props) {
   return (
     <>
-      <ul>
+      <List>
         {products.map((product) => (
-          <li key={product.id}>
-            {product.name} - {product.price}
-          </li>
+          <ListItem key={product.id}>
+            <ListItemAvatar>
+              <Avatar src={product.pictureUrl} />
+            </ListItemAvatar>
+            <ListItemText>
+              {product.name} - {product.price}
+            </ListItemText>
+          </ListItem>
         ))}
-      </ul>
-      <button onClick={addProduct}>Add Product</button>
+      </List>
+      <Button variant="contained" onClick={addProduct}>Add Product</Button>
     </>
   );
 }
