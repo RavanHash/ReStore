8482e67d01e45bc58387a54fda418b9145e2c185
Ravan <ravanhashimov@gmail.com>
2023-07-01 04:33:38 -0700

    Add simple Product component

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { Product } from "../models/product";
+import Catalog from "../../features/catalog/Catalog";
+
+function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  function addProduct() {
+    setProducts((prevSate) => [
+      ...prevSate,
+      {
+        id: prevSate.length + 101,
+        name: "C" + (prevSate.length + 1),
+        price: prevSate.length * 100,
+        brand: "smth",
+        description: "smth",
+        pictureUrl: "http://picsum.photos/200",
+      },
+    ]);
+  }
+
+  return (
+    <div>
+      <h1>Re-Store</h1>
+      <Catalog products={products} addProduct={addProduct}/>
+    </div>
+  );
+}
+
+export default App;
